f8cdf4a78220fb24812dc6bb778070444e4a0284
llHoYall <hoya128@gmail.com>
2018-03-26 09:33:53 +0900

    Update project: Weather (React-Native)
    - Get weather information based on location

--- ReactNative_Weather/App.js
+++ ReactNative_Weather/App.js
@@ -2,21 +2,51 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from "./Weather";
 
+const API_KEY = "54ad201e9fb53b07841b1c60f44e16b6";
+
 export default class App extends React.Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
   };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+        this.setState({
+          error: error
+        })
+      }
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+      });
+  }
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         {/* <StatusBar barStyle="light-content" /> */}
         <StatusBar hidden={true} />
         {isLoaded ? (<Weather/>) : (
           <View style={styles.loading}>
-            <Text style={styles.loadingText}>
-              Getting the Weather
-            </Text>
+            <Text style={styles.loadingText}>Getting the Weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -37,6 +67,11 @@ const styles = StyleSheet.create({
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginBottom: 24
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   }
 });
