76e6de6deeeb6e97ea299a0545663c3fdf84fcbf
llHoYall <hoya128@gmail.com>
2018-03-29 22:10:40 +0900

    Update project: Movie (React)
    - Update title, poster list to single list using map

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Avengers: Infinity War"
-]
-
-const movieImages = [
-  "https://ia.media-imdb.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+const movies = [
+  {
+    title: "Avengers: Infinity War",
+    poster: "https://ia.media-imdb.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
