6b1dff765d1145ecbf465e15fcd5b97f30466d01
llHoYall <hoya128@gmail.com>
2018-03-30 22:09:35 +0900

    Update project: Movie (React)
    - Using state. If state is changed, update rendering

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -10,10 +10,36 @@ const movies = [
 ]
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {
+    movies: [
+      {
+        title: "Avengers: Infinity War",
+        poster: "https://ia.media-imdb.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+      }
+    ]
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Deadpool 2",
+            poster: "https://c1.staticflickr.com/5/4666/26350689038_758a159d63_b.jpg"
+          }
+        ]
+      })
+    }, 3000)
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
+        {this.state.movies.map(movie => {
           return <Movie title={movie.title} poster={movie.poster} />
         })}
       </div>
