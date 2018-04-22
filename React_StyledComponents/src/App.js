df3e1c98459687a72b93814429f7d84714b5a730
llHoYall <hoya128@gmail.com>
2018-04-22 20:58:40 +0900

    Update project: Styled-Components (React)
    - Theming

--- React_StyledComponents/src/App.js
+++ React_StyledComponents/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled, { injectGlobal, css, keyframes } from "styled-components";
+import styled, { injectGlobal, css, keyframes, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 injectGlobal`
   body {
@@ -14,33 +15,53 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+// const awesomeCard = css`
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   background-color: white;
+//   border-radius: 10px;
+//   padding: 20px;
+// `;
+
+// const Input = styled.input.attrs({
+//   required: true
+// })`
+//   border: none;
+//   ${awesomeCard};
+// `;
+
+const Card = styled.div`
+  background-color: red;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  ${awesomeCard};
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        {/* <Button>Hello</Button>
-        <Button danger>Hello</Button>
-        <Anchor href="http://google.com">Go to Google</Anchor> */}
+      <ThemeProvider theme={theme}>
+        <Container>
+          {/* <Button>Hello</Button>
+          <Button danger>Hello</Button>
+          <Anchor href="http://google.com">Go to Google</Anchor> */}
 
-        <Input placeholder="hello" />
-      </Container>
+          {/* <Input placeholder="hello" /> */}
+
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 // const Button = styled.button`
 //   border-radius: 50px;
