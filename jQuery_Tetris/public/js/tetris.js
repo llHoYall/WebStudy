3b300d0101a39bddce35e68d87e6e0bbee3a6f58
llHoYall <hoya128@gmail.com>
2018-04-23 22:06:17 +0900

    Update project: Tetris (jQuery)
    - Create block

--- jQuery_Tetris/public/js/tetris.js
+++ jQuery_Tetris/public/js/tetris.js
@@ -1,7 +1,27 @@
+function setType(typeName) {
+	var type = types[typeName];
+	var width = type.width;
+	var height = type.height;
+
+	$("#handle").empty().width(width * 25).height(height * 25);
+
+	for (var i = 0; i < height; i++) {
+		for (var j = 0; j < width; j++) {
+			if (type.shape[i][j]) {
+				$("<div>").addClass(typeName).addClass("block").appendTo("#handle");
+			}
+			else {
+				$("<div>").addClass("whiteLine").addClass("block").appendTo("#handle");
+			}
+		}
+	}
+}
+
 $(document).ready(function () {
 	for (var i = 0; i < 20; i++) {
 		for (var j = 0; j < 10; j++) {
 			$("<div>").addClass("block none").appendTo("#stage");
 		}
 	}
+	setType("type2");
 });
