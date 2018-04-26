1398aa77adbf29986a5468cbf35da77ae3297b57
llHoYall <hoya128@gmail.com>
2018-04-26 21:41:57 +0900

    Update project: Bingo (jQuery)
    - Refactoring to MVC pattern.

--- jQuery_Bingo/public/js/bingo.js
+++ jQuery_Bingo/public/js/bingo.js
@@ -1,21 +1,47 @@
-let Bingo = function(player) {
-  let p = $(player);
-  let numbers;
+// Model
+let BingoModel = function(number) {
+  let attributes = {
+    number,
+    selected: false
+  };
+
+  this.get = attr => {
+    if (attributes[attr]) {
+      return attributes[attr];
+    } else {
+      return null;
+    }
+  };
+
+  this.set = (attr, value) => {
+    attributes[attr] = value;
+    $(this).trigger("change");
+  };
+
+  this.select = () => {
+    this.set("selected", true);
+  };
+};
+
+// Collection
+let BingoCollection = function() {
+  this.models = [];
 
   this.init = () => {
-    numbers = [];
+    let self = this;
+
+    let numbers = [];
     for (let i = 1; i <= 25; i++) {
       numbers.push(i);
     }
     numbers = getRandomSet(numbers);
 
-    this.render();
-  };
-
-  this.render = () => {
-    p.find("td").each(function(i) {
-      $(this).text(numbers[i]);
-    });
+    for (let i = 0, length = numbers.length; i < length; i++) {
+      this.models.push(new BingoModel(numbers[i]));
+      $(this.models[i]).on("change", function() {
+        $(self).trigger("update");
+      });
+    }
   };
 
   let getRandomSet = numberSet => {
@@ -29,10 +55,44 @@ let Bingo = function(player) {
   };
 };
 
+// View
+let BingoView = function(player) {
+  let p = $(player);
+  let collection = null;
+
+  this.init = () => {
+    collection = new BingoCollection(p);
+    collection.init();
+
+    this.render();
+
+    p.find("td").on("click", onClick);
+    $(collection).on("update", this.render);
+  };
+
+  let onClick = function(event) {
+    let model_id = $(this).attr("model");
+    collection.models[model_id].select();
+  };
+
+  this.render = () => {
+    p.find("td").each(function(i) {
+      $(this)
+        .attr("model", i)
+        .text(collection.models[i].get("number"));
+      if (collection.models[i].get("selected")) {
+        $(this).addClass("selected");
+      } else {
+        $(this).removeClass("selected");
+      }
+    });
+  };
+};
+
 $(() => {
-  let player1 = new Bingo("#player1");
+  let player1 = new BingoView("#player1");
   player1.init();
 
-  let player2 = new Bingo("#player2");
+  let player2 = new BingoView("#player2");
   player2.init();
 });
