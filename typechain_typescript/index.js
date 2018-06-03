a069cb694ad4832e546bdab361d19c2740bf22ea
llHoYall <hoya128@gmail.com>
2018-06-03 22:15:35 +0900

    Update project: TypeChain (TypeScript)
    - Optional arguments.

--- typechain_typescript/index.js
+++ typechain_typescript/index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "HoYa", age = 37, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, ${age}, ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- typechain_typescript/index.ts
+++ typechain_typescript/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "HoYa",
+age = 37,
+gender = "male"
+
+// Optional arguments
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, ${age}, ${gender}`)
+}
+sayHi(name, age)
+
+export {}
